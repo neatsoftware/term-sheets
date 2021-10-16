f208d1357d94ca9877ed6429778e9c59c2c3dece
gpoitch <garth22@gmail.com>
2021-10-16 17:24:41 -0400

    deploy

--- .postcssrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  modules: true,
-  plugins: {
-    autoprefixer: true,
-    'postcss-modules': {
-      generateScopedName: process.env.NODE_ENV === 'production' ? '[hash:base64:3]' : '[local]__[hash:base64:3]'
-    }
-  }
-}
--- src/demos/index.js
+++
@@ -1,4 +0,0 @@
-export { default as createReactApp } from './create-react-app'
-export { default as sammie } from './sammie'
-export { default as featureTests } from './feature-tests'
-export { default as createAngularApp } from './create-angular-app'
